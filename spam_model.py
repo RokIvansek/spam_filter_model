ab7b21ba7cd4c82c87d8ae206f47a7e2c243d971
RokIvansek <ivansek.rok@gmail.com>
2017-07-16 13:04:41 +0200

    added function for predicting a single string

--- spam_model.py
+++ spam_model.py
@@ -25,7 +25,7 @@ def parse_data(save_to_cwd=False):
     if save_to_cwd:
         np.save("X_raw", np.array(X))
         np.save("Y_raw", np.array(Y))
-    return (X, Y)
+    return X, Y
 
 def load_from_npy_file(path_to_X_and_Y):
     X = np.load(path_to_X_and_Y + "/X_raw.npy")
@@ -40,7 +40,7 @@ def extract_features(X_train):
     count_vect = CountVectorizer()
     X_train_counts = count_vect.fit_transform(X_train)
     # print(X_train_counts.shape)
-    return(X_train_counts, count_vect)
+    return X_train_counts, count_vect
 
 # This makes the counts too close to zero and pca doesn't work then
 # TODO: You should find a smarter way to incorporate length of string into features
@@ -51,35 +51,39 @@ def devide_by_log_length_of_string(X, X_features):
     # print(X_features.shape)
     for i in range(n):
         X_features[i,:] = X_features[i,:]/np.log(len(X[i]))
-    return(X_features)
+    return X_features
 
 def standard_scaler(X_train):
     ss = StandardScaler()
     X_train_scaled = ss.fit_transform(X_train.toarray())
-    return (X_train_scaled, ss)
+    return X_train_scaled, ss
 
-def pca(X_train):
-    pca_processor = PCA(n_components=100)
+def pca(X_train, n_comp=100):
+    pca_processor = PCA(n_components=n_comp)
     X_train_pca = pca_processor.fit_transform(X_train)
-    # print(np.sum(pca_processor.explained_variance_ratio_))
-    return(X_train_pca, pca_processor)
+    # print("Selected components explain " +
+    #       str(int(100*np.round(np.sum(pca_processor.explained_variance_ratio_), decimals=2))) +
+    #       " percent of the variance.")
+    return X_train_pca, pca_processor
 
 def pre_process(X, count_vect, ss, pca):
-    return(pca.transform(ss.transform(count_vect.transform(X).toarray())))
+    if type(X) == str: # If input is a single string
+        return pca.transform(ss.transform(count_vect.transform(np.array([X])).toarray()))
+    else: # If we have a matrix
+        return pca.transform(ss.transform(count_vect.transform(X).toarray()))
 
 def train_model(X, y):
-    rf = RandomForestClassifier(n_estimators=1000, n_jobs=-1)
+    rf = RandomForestClassifier(n_estimators=2000, n_jobs=-1)
     rf.fit(X, y)
-    return (rf)
+    return rf
 
 def predict_on_X_test(X_test, model):
     y_test_predict = model.predict(X_test)
-    return (y_test_predict)
+    return y_test_predict
 
-
-def predict_a_single_string(s, preprocessor, model):
-    pass
-    # TODO: Use the feature extractor and the model to make a prediction about a string
+def predict_a_single_string(s, count_vect, ss, pca, model):
+    s_preprocessed = pre_process(s, count_vect, ss, pca)
+    return model.predict(s_preprocessed)[0]
 
 if __name__ == '__main__':
     # X, Y = parse_data(save_to_cwd=True)
@@ -88,9 +92,14 @@ if __name__ == '__main__':
     X_train_features, count_vect = extract_features(X_train)
     # X_train_features_fixed = devide_by_log_length_of_string(X_train, X_train_features)
     X_train_scaled, ss = standard_scaler(X_train_features)
-    X_train_pca, pca_processor = pca(X_train_scaled)
+    X_train_pca, pca_processor = pca(X_train_scaled, 100)
     X_test_pca = pre_process(X_test, count_vect, ss, pca_processor)
     rf_model = train_model(X_train_pca, y_train)
     y_test_prediction = predict_on_X_test(X_test_pca, rf_model)
     print(accuracy_score(y_test, y_test_prediction))
+    # Single string prediction
+    s_genuine = "Pokemon master Rok is the best pokemon master"
+    s_spam = "Call now and win a 2000 dollar prize or visit www.free2000dollars.com."
+    predict_a_single_string(s_genuine, count_vect, ss, pca_processor, rf_model)
+    predict_a_single_string(s_spam, count_vect, ss, pca_processor, rf_model)
 
